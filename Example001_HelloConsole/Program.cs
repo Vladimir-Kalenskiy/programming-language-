2bb4f40f9c0a8b46d6317da7af2a5f8dbd8ea61e
Владимир Каленский <kalenskiy.man@icloud.com>
2022-10-12 23:56:52 +0300

    add Example004 Div

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,2 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string unername = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine("username");
 // Console - это окно терминалf; WriteLine - это вывести в окно терминала; (") - что вывести.
