7bd9a41e552c6274f3d29aaa752a6ab5e8ff2f95
Владимир Каленский <kalenskiy.man@icloud.com>
2022-10-13 19:48:31 +0300

    add Example007 Magic

---
+++ Example005_Condition_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользовотеля: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "галя")
+{
+    Console.WriteLine("Ура, это же Галя!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
